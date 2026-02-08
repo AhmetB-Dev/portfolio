7d09f63f31d3467407e94929adaefb875846bec9
AhmetB-Dev <ahmet_ba@web.de>
2026-02-08 19:20:12 +0100

    feat: enhance header component with Angular routing and improve styles for better navigation experience

--- portfolio-app/src/app/core/header/header.ts
+++ portfolio-app/src/app/core/header/header.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLinkActive, RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLinkActive, RouterLinkWithHref],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
-
-}
+export class Header {}
